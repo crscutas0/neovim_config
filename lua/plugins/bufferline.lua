4833a05b73ce7aa13ca97b87a5b5cc21fad055b6
Cristopher Cutas <crscutas00@gmail.com>
2025-08-15 18:32:42 +0800

    added bufferline

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,36 @@
+return {
+  "akinsho/bufferline.nvim",
+  lazy = false,
+  keys = {
+    { "<S-x>", "<cmd>BufferLineCloseOthers<cr>", desc = "Delete Buffers to the Left" },
+    { "<S-h>", "<cmd>BufferLineCyclePrev<cr>",   desc = "Prev Buffer" },
+    { "<S-l>", "<cmd>BufferLineCycleNext<cr>",   desc = "Next Buffer" },
+  },
+  opts = {
+    options = {
+      diagnostics = "nvim_lsp",
+      always_show_bufferline = false,
+      offsets = {
+        {
+          filetype = "neo-tree",
+          text = "Neo-tree",
+          highlight = "Directory",
+          text_align = "left",
+        },
+        {
+          filetype = "snacks_layout_box",
+        },
+      },
+    },
+  },
+  config = function(_, opts)
+    require("bufferline").setup(opts)
+    vim.api.nvim_create_autocmd({ "BufAdd", "BufDelete" }, {
+      callback = function()
+        vim.schedule(function()
+          pcall(nvim_bufferline)
+        end)
+      end,
+    })
+  end,
+}
