9f1a3dba34c9d65e97f0473c5c1365b2a91d9c1e
Cristopher Cutas <crscutas00@gmail.com>
2025-08-18 08:55:58 +0800

    added gitignore in neotree

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -1,25 +1,25 @@
 return {
-  {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "MunifTanjim/nui.nvim",
-      "nvim-tree/nvim-web-devicons", -- optional, but recommended
-    },
-    lazy = false,
-    opts = {
-      filesystem = {
-        filtered_items = {
-          always_show = { ".env" },
-        }
-      }
-    },
-    keys = {
-      { "<leader>et", "<cmd>Neotree toggle<cr>", desc = "Toggle Neo-tree" },
-      { "<leader>ee", "<cmd>Neotree focus<cr>",  desc = "Focus Neo-tree" },
-      -- { "<leader>s", "<cmd>Neotree show<cr>",  desc = "Show Neo-tree (no focus)" },
-      -- { "<leader>c", "<cmd>Neotree close<cr>", desc = "Close Neo-tree" },
-    },
-  }
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			"nvim-tree/nvim-web-devicons", -- optional, but recommended
+		},
+		lazy = false,
+		opts = {
+			filesystem = {
+				filtered_items = {
+					always_show = { ".env", ".gitignore" },
+				},
+			},
+		},
+		keys = {
+			{ "<leader>et", "<cmd>Neotree toggle<cr>", desc = "Toggle Neo-tree" },
+			{ "<leader>ee", "<cmd>Neotree focus<cr>", desc = "Focus Neo-tree" },
+			-- { "<leader>s", "<cmd>Neotree show<cr>",  desc = "Show Neo-tree (no focus)" },
+			-- { "<leader>c", "<cmd>Neotree close<cr>", desc = "Close Neo-tree" },
+		},
+	},
 }
