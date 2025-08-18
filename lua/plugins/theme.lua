771e173fa13bd511fed9faf21c69ef804245cd51
Cristopher Cutas <crscutas00@gmail.com>
2025-08-18 11:27:30 +0800

    making line number pretty

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -4,11 +4,16 @@ return {
 		name = "catppuccin",
 		priority = 1000,
 		opts = {
-			flavour = 'frappe'
+			flavour = "frappe",
+			custom_highlights = function()
+				return {
+					CursorLineNr = { fg = "#ffbf00", bold = true },
+				}
+			end,
 		},
 		config = function(_, opts)
 			require("catppuccin").setup(opts)
 			vim.cmd.colorscheme("catppuccin")
-		end
+		end,
 	},
 }
