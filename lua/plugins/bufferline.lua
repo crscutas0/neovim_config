5dde0a43a7ca7c859ce9083c06c11b9fa46bbe0f
Cristopher Cutas <crscutas00@gmail.com>
2025-08-28 10:49:54 +0800

    shift + z, shift + c to close buffer left and right

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -1,36 +1,38 @@
 return {
-  "akinsho/bufferline.nvim",
-  lazy = false,
-  keys = {
-    { "<S-x>", "<cmd>BufferLineCloseOthers<cr>", desc = "Delete Buffers to the Left" },
-    { "<S-h>", "<cmd>BufferLineCyclePrev<cr>",   desc = "Prev Buffer" },
-    { "<S-l>", "<cmd>BufferLineCycleNext<cr>",   desc = "Next Buffer" },
-  },
-  opts = {
-    options = {
-      diagnostics = "nvim_lsp",
-      always_show_bufferline = false,
-      offsets = {
-        {
-          filetype = "neo-tree",
-          text = "Neo-tree",
-          highlight = "Directory",
-          text_align = "left",
-        },
-        {
-          filetype = "snacks_layout_box",
-        },
-      },
-    },
-  },
-  config = function(_, opts)
-    require("bufferline").setup(opts)
-    vim.api.nvim_create_autocmd({ "BufAdd", "BufDelete" }, {
-      callback = function()
-        vim.schedule(function()
-          pcall(nvim_bufferline)
-        end)
-      end,
-    })
-  end,
+	"akinsho/bufferline.nvim",
+	lazy = false,
+	keys = {
+		{ "<S-z>", "<cmd>BufferLineCloseLeft<cr>", desc = "Close All Buffers to the Left" },
+		{ "<S-c>", "<cmd>BufferLineCloseRight<cr>", desc = "Close All Buffers to the Right" },
+		{ "<S-x>", "<cmd>BufferLineCloseOthers<cr>", desc = "Delete Buffers to the Left" },
+		{ "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+		{ "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+	},
+	opts = {
+		options = {
+			diagnostics = "nvim_lsp",
+			always_show_bufferline = false,
+			offsets = {
+				{
+					filetype = "neo-tree",
+					text = "Neo-tree",
+					highlight = "Directory",
+					text_align = "left",
+				},
+				{
+					filetype = "snacks_layout_box",
+				},
+			},
+		},
+	},
+	config = function(_, opts)
+		require("bufferline").setup(opts)
+		vim.api.nvim_create_autocmd({ "BufAdd", "BufDelete" }, {
+			callback = function()
+				vim.schedule(function()
+					pcall(nvim_bufferline)
+				end)
+			end,
+		})
+	end,
 }
