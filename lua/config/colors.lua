309a8e63b1f5bd69b5e170030a68a0e1860a4445
Cristopher Cutas <crscutas00@gmail.com>
2025-09-12 16:07:37 +0800

    added other colors

---
+++ lua/config/colors.lua
@@ -0,0 +1,11 @@
+-- The following are the themes installed in this configuration
+-- you can switch themes by change colorscheme value.
+--
+-- After changing execute :source
+
+--- catppuccin
+--- nordic
+--- everforest
+--- onedark
+
+vim.cmd.colorscheme("onedark")
--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -13,7 +13,54 @@ return {
 		},
 		config = function(_, opts)
 			require("catppuccin").setup(opts)
-			vim.cmd.colorscheme("catppuccin")
+		end,
+	},
+	{
+		"AlexvZyl/nordic.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("nordic").setup({
+				cursorline = {
+					bold = true,
+					bold_number = true,
+					theme = "dark",
+					blend = 0.85,
+				},
+			})
+			vim.defer_fn(function()
+				vim.api.nvim_set_hl(0, "CursorLineNr", { fg = "#ffbf00", bold = true })
+			end, 50)
+		end,
+	},
+	{
+		"neanias/everforest-nvim",
+		version = false,
+		lazy = false,
+		priority = 1000,
+
+		config = function()
+			require("everforest").setup({
+				background = "soft",
+				on_highlights = function(highlights, _)
+					highlights.LineNr = { fg = "#ffbf00", bold = true }
+					highlights.CursorLineNr = { fg = "#ffbf00", bold = true }
+				end,
+			})
+		end,
+	},
+	{
+		"navarasu/onedark.nvim",
+		priority = 1000,
+		config = function()
+			require("onedark").setup({
+				style = "warm",
+				colors = {},
+				highlights = {
+					CursorLineNr = { fg = "#ffbf00", fmt = "bold" },
+				},
+			})
+			require("onedark")
 		end,
 	},
 }
