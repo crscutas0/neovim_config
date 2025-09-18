df70ec4947948b1313549d80c91dd3b1cd121dd6
Cristopher Cutas <crscutas00@gmail.com>
2025-09-18 17:19:47 +0800

    fix issue on rename

--- lua/plugins/mini-indent.lua
+++ lua/plugins/mini-indent.lua
@@ -8,6 +8,9 @@ return {
 				animation = require("mini.indentscope").gen_animation.linear({ duration = 50, unit = "total" }),
 				-- animation = require("mini.indentscope").gen_animation.none(),
 				predicate = function(scope)
+					if not vim.api.nvim_buf_is_valid(scope.buf_id) then
+						return false
+					end
 					if vim.bo[scope.buf_id].filetype == "neo-tree" then
 						return false
 					end
