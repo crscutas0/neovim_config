6fcf16968f721055817dcb6a990dfd736638a676
Cristopher Cutas <crscutas00@gmail.com>
2025-10-30 10:44:49 +0800

    update neotree, dashboard, and mappings

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -23,6 +23,32 @@ return {
 			window = {
 				width = 40,
 			},
+			event_handlers = {
+				{
+					event = "neo_tree_buffer_enter",
+					handler = function()
+						if vim.bo.filetype ~= "neo-tree" then
+							return
+						end
+
+						vim.api.nvim_create_autocmd("CursorMoved", {
+							buffer = 0,
+							callback = function()
+								local line = vim.api.nvim_get_current_line()
+								local row = vim.api.nvim_win_get_cursor(0)[1]
+
+								local start = line:find("[%w._%-%+~]") or line:find("%S") or 1
+								local col = math.max(0, start - 1)
+
+								local _, current_col = unpack(vim.api.nvim_win_get_cursor(0))
+								if current_col ~= col then
+									pcall(vim.api.nvim_win_set_cursor, 0, { row, col })
+								end
+							end,
+						})
+					end,
+				},
+			},
 		},
 		keys = require("config.keymap.neotree"),
 	},
