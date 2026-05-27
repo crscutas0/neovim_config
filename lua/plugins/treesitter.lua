a5791947dcffa86b055d70611fa4d845c1dd6634
Cristopher Cutas <crscutas00@gmail.com>
2026-05-28 00:48:27 +0800

    fix tree sitter back

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,4 +1,29 @@
 return {
-	"nvim-treesitter/nvim-treesitter",
-	build = ":TSUpdate",
+	{
+		"nvim-treesitter/nvim-treesitter",
+		lazy = false,
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter").install({
+				ensure_installed = {
+					"java",
+					"lua",
+					"go",
+					"python",
+					"typescript",
+					"javascript",
+					"tsx",
+					"html",
+					"htmldjango",
+					"css",
+					"markdown",
+					"json",
+					"svelte",
+					"sql",
+				},
+				highlight = { enable = true },
+				indent = { enable = true },
+			})
+		end,
+	},
 }
