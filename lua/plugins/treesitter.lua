0cc1400a0eaaceb95be7c24901f1abd73a28ff2f
Cristopher Cutas <crscutas00@gmail.com>
2026-05-28 00:37:36 +0800

    finally fixed the svelte

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,29 +1,4 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		lazy = false,
-		build = ":TSUpdate",
-		config = function()
-			require("nvim-treesitter").install({
-				ensure_installed = {
-					"java",
-					"lua",
-					"go",
-					"python",
-					"typescript",
-					"javascript",
-					"tsx",
-					"html",
-					"htmldjango",
-					"css",
-					"markdown",
-					"json",
-					"svelte",
-					"sql",
-				},
-				highlight = { enable = true },
-				indent = { enable = true },
-			})
-		end,
-	},
+	"nvim-treesitter/nvim-treesitter",
+	build = ":TSUpdate",
 }
