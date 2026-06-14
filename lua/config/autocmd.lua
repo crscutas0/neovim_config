da6546b0eca279d1fc80bb72b64e439843dcaf8d
Cristopher Cutas <crscutas00@gmail.com>
2026-06-14 15:22:55 +0800

    remove lsp colors for tailwind

--- lua/config/autocmd.lua
+++ lua/config/autocmd.lua
@@ -4,3 +4,13 @@ vim.api.nvim_create_autocmd("FileType", {
 		pcall(vim.treesitter.start)
 	end,
 })
+
+vim.api.nvim_create_autocmd("LspAttach", {
+	callback = function(args)
+		local client = vim.lsp.get_client_by_id(args.data.client_id)
+		if client and client.name == "tailwindcss" then
+			-- Turn off the native document color handler for Tailwind
+			vim.lsp.document_color.enable(false, { bufnr = args.buf })
+		end
+	end,
+})
